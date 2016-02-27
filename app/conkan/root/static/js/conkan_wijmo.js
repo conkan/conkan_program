6a7ddf79293c093ad95d3624649efa146c201ef8
REM <rem@s-rem.jp>
2016-02-27 16:30:49 +0900

    wijGridのスクロールサイズ Fix

--- app/conkan/root/static/js/conkan_wijmo.js
+++ app/conkan/root/static/js/conkan_wijmo.js
@@ -1,20 +1,20 @@
 var wijGridResize = function () {
-  <!-- dataGridのstyle:heightを、計算し設定 -->
   var
   h,
   wh = window.innerHeight || $(window).innerHeight(),
   pgldiv = $("#gridlist"),
   wijdiv = $("#gridlist .wijgridouterdiv"),
+  scldiv = $("#gridlist .wijmo-wijgrid-scroller"),
   adddiv = $("#pgAddDiv");
-  h  = wh - ( pgldiv.offset().top +
-              parseFloat(pgldiv.css('paddingTop')) +
-              parseFloat(pgldiv.css('paddingBottom')) +
-              parseFloat(pgldiv.css('borderTopWidth')) +
-              parseFloat(pgldiv.css('borderBottomWidth')) +
-              parseFloat(pgldiv.css('marginTop')) +
-              parseFloat(pgldiv.css('marginBottom')) +
-              parseFloat(wijdiv.css('borderTopWidth')) +
-              parseFloat(wijdiv.css('borderBottomWidth')) +
-              adddiv.outerHeight() );
-  wijdiv.height(h + 'px');
+  h    = wh - ( pgldiv.offset().top +
+                parseFloat(pgldiv.css('paddingTop')) +
+                parseFloat(pgldiv.css('paddingBottom')) +
+                parseFloat(pgldiv.css('borderTopWidth')) +
+                parseFloat(pgldiv.css('borderBottomWidth')) +
+                parseFloat(pgldiv.css('marginTop')) +
+                parseFloat(pgldiv.css('marginBottom')) +
+                parseFloat(wijdiv.css('borderTopWidth')) +
+                parseFloat(wijdiv.css('borderBottomWidth')) +
+                adddiv.outerHeight() );
+  scldiv.css('max-height', h + 'px');
 };
