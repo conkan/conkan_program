a1d935d8f54d9104c887857ceaddaacbecd6d58e
REM <rem@s-rem.jp>
2016-12-09 16:25:29 +0900

    conkanのみのコンテナ起動 要らないファイルとかがまだあると思います

--- run.sh
+++ run.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
+NAME='conkanprog'
+PSET='-p 9002:9002'
+RUNOPT='-d --restart=always'
+LOGMNT='-v /var/log/conkanprog:/var/log/conkanprog'
 # 本番系は、run.sh product で起動
 if [ "$1" = "product" ] ; then
-    HP='80'
-    HS='443'
-    MS='--link mysql'
+    DEVMNT=''
 else
-    HP='30080'
-    HS='30443'
+    # 開発時はgit checkout先をmountする
+    DEVMNT='-v '$(pwd)'/app:/root/app'
+    # 存在しない場合、conkan.ymlを初期化(でないと起動失敗する)
+    if ! [ -e `pwd`/app/conkan/conkan.yml ]; then
+        cp `pwd`/app/conkan/conkan.yml_default `pwd`/app/conkan/conkan.yml
+    fi
 fi
-docker stop conkan
-docker rm conkan
-# 存在しない場合、conkan.ymlを初期化(でないと起動失敗する)
-if ! [ -e `pwd`/app/conkan/conkan.yml ]; then
-  cp `pwd`/app/conkan/conkan.yml_default `pwd`/app/conkan/conkan.yml
+STAT=`docker inspect $NAME | grep Status | awk -F'"' '{print $4}'`
+if [ !${STAT} ]; then
+    STAT=`docker inspect $NAME | grep Running | awk '{print $2}'`
+    if [ ${STAT} == 'true,' ]; then
+        STAT='running'
+    fi
 fi
-docker run -d --restart='always' $MS --name conkan -p $HP:80 -p $HS:443 -v `pwd`/app:/root/app srem/conkan
+if [ ${STAT} ]; then
+    if [ ${STAT} == 'running' ]; then
+        docker stop ${NAME}
+    fi
+    docker rm ${NAME}
+fi
+docker run ${RUNOPT} --name ${NAME} ${PSET} ${LOGMNT} ${DEVMNT} conkan/conkan_program
