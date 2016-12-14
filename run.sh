4487b01a51bdea182c98d1293c4d9b63c5008f14
REM <rem@s-rem.jp>
2016-12-14 11:20:10 +0900

    git cloneしない環境でも動作するよう修正

--- run.sh
+++ run.sh
@@ -7,12 +7,19 @@ PSET='-p 127.0.0.1:9002:9002'
 ENVOPT="-e HOSTNAME=${NAME}"
 RUNOPT='-d --restart=always'
 LOGMNT='-v /var/log/conkanprog:/var/log/conkanprog'
-# 開発時はgit checkout先のappをmountする(DEVTOPに指定)
-DEVTOP="$(pwd)/app"
-DEVMNT="-v ${DEVTOP}:/root/app"
-# 本番系は、run.sh product で起動
+
+# git cloneしたディレクトリを指定(本番系でDockerのみ使う場合は不要)
+GITTOP="$(pwd)"
+# 開発時はgit checkout先のappをmountする
+DEVMODE=1
+DEVMNT="-v ${GITTOP}/app:/root/app"
+
 if [ $1 ]; then
+    # 本番系は、run.sh product で起動
     if [ $1 = "product" ] ; then
+        DEVMODE=''
+        IMGTAG=':1.0.0'
+        # backupとconkan.ymlを永続化する
         if ! [ -d $(pwd)/conkan ]; then
             mkdir $(pwd)/conkan 
         fi
@@ -20,30 +27,28 @@ if [ $1 ]; then
             mkdir $(pwd)/backup 
         fi
         if ! [ -e $(pwd)/conkan/conkan.yml ]; then
-            if [ -e ${DEVTOP}/conkan/conkan.yml ]; then
-                cp ${DEVTOP}/conkan/conkan.yml $(pwd)/conkan/conkan.yml
-            elif [ -e ${DEVTOP}/conkan/conkan.yml_default ]; then
-                cp ${DEVTOP}/conkan/conkan.yml_default $(pwd)/conkan/conkan.yml
+            # 開発系で起動したものを本番系に移行する時、
+            # 既存の設定ファイルを流用する
+            if [ -e ${GITTOP}/app/conkan/conkan.yml ]; then
+                cp ${GITTOP}/app/conkan/conkan.yml $(pwd)/conkan/conkan.yml
             else
-                echo "Cannot start ConkanProgram."
-                echo "Please create $(pwd)/conkan/conkan.yml"
-                echo "(SEE https://conkan.github.io/foruser/devops.html)"
-                exit
+                docker run -i -t conkan/conkan_program${IMGTAG} cat /root/app/conkan/conkan.yml_default > $(pwd)/conkan/conkan.yml
             fi
         fi
-        DEVTOP="$(pwd)"
-        DEVMNT="-v ${DEVTOP}/backup:/root/app/backup -v ${DEVTOP}/conkan/conkan.yml:/root/app/conkan/conkan.yml"
-        IMGTAG=':1.0.0'
+        DEVMNT="-v $(pwd)/backup:/root/app/backup -v $(pwd)/conkan/conkan.yml:/root/app/conkan/conkan.yml"
     fi
     # デバッグ時はconkan_server.plを起動
     if [ $1 = "debug" ] ; then
         RUNCMD='/root/app/conkan/script/conkan_server.pl -r -d -p 9002'
     fi
 fi
-# 存在しない場合、conkan.ymlを初期化(でないと起動失敗する)
-if ! [ -e ${DEVTOP}/conkan/conkan.yml ]; then
-    cp ${DEVTOP}/conkan/conkan.yml_default ${DEVTOP}/conkan/conkan.yml
+# 開発時 存在しない場合、conkan.ymlを初期化(でないと起動失敗する)
+if [ ${DEVMODE} ]; then
+    if ! [ -e ${GITTOP}/app/conkan/conkan.yml ]; then
+        cp ${GITTOP}/app/conkan/conkan.yml_default ${GITTOP}/app/conkan/conkan.yml
+    fi
 fi
+
 MNTOPT="${LOGMNT} ${DEVMNT}"
 
 STAT=`docker inspect ${NAME} | grep Status | awk -F'"' '{print $4}'`
@@ -64,5 +69,5 @@ if [ ${STAT} ]; then
     docker rm ${NAME}
 fi
 ALLOPT="${RUNOPT} --name ${NAME} ${PSET} ${MNTOPT} ${LNKOPT} ${ENVOPT}"
-echo "docker run ${ALLOPT} conkan/conkan_program ${RUNCMD}"
+echo "docker run ${ALLOPT} conkan/conkan_program${IMGTAG} ${RUNCMD}"
 docker run ${ALLOPT} conkan/conkan_program${IMGTAG} ${RUNCMD}
